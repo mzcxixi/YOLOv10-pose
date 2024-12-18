6d466276bb1424886d55a18cfe7ee2038dd86e18
mzcxixi <93956462+mzcxixi@users.noreply.github.com>
2024-12-18 15:42:14 +0800

    Update predict_pose.py

--- predict_pose.py
+++ predict_pose.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLOv10
-model = YOLOv10('/home/mzc/YOLOv10-pose/runs/v10-pose/train18/weights/last.pt')
+model = YOLOv10('runs/v10-pose/train18/weights/last.pt')
 model.predict(source='valid/images', save=True)
